a8eb8b6134a5fde251b7a6b6c604f111fd8f48d5
shell-gal <499867382@qq.com>
2019-12-18 10:32:30 +0800

    cq

--- src/main/java/com/hnist/hotel/service/impls/HotelServiceImpl.java
+++ src/main/java/com/hnist/hotel/service/impls/HotelServiceImpl.java
@@ -3,12 +3,19 @@ package com.hnist.hotel.service.impls;
 import com.alibaba.druid.sql.PagerUtils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.hnist.hotel.mapper.CityMapper;
 import com.hnist.hotel.mapper.HotelMapper;
+import com.hnist.hotel.mapper.RoomTypeMapper;
+import com.hnist.hotel.pojo.City;
 import com.hnist.hotel.pojo.Hotel;
 import com.hnist.hotel.pojo.PageParams;
 import com.hnist.hotel.pojo.PageResult;
 import com.hnist.hotel.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import tk.mybatis.mapper.entity.Example;
 
 import javax.annotation.Resource;
@@ -19,14 +26,21 @@ public class HotelServiceImpl implements HotelService {
 
     @Resource
     HotelMapper hotelMapper;
+
+    @Resource
+    CityMapper cityMapper;
+
     @Override
-    public PageInfo queryallhotel(Integer indexpage,String city) {
+    public PageInfo queryallhotel(Integer indexpage,String city,Integer userid) {
         if (indexpage==null){
             indexpage=1;
         }
         Example example=new Example(Hotel.class);
-        example.createCriteria().andEqualTo("city",city);
-        PageHelper.startPage(indexpage,9);
+        Example.Criteria criteria = example.createCriteria();
+        if (city!=null&&!city.equals("null")){
+            criteria.andEqualTo("city",city);
+        }
+        PageHelper.startPage(indexpage,8);
         List<Hotel> hotelList = hotelMapper.selectByExample(example);
         PageInfo pageInfo=new PageInfo(hotelList,4);
         if (hotelList!=null&&hotelList.size()>0){
@@ -58,22 +72,47 @@ public class HotelServiceImpl implements HotelService {
     }
 
     @Override
-    public boolean delete(Integer hotelid) {
-        if (hotelid!=null){
-            int i = hotelMapper.deleteByPrimaryKey(hotelid);
+    @Transactional
+    public boolean delete(@RequestBody Hotel hotel) {
+        if (hotel!=null){
+            int i = hotelMapper.delete(hotel);
             if (i>0){
-                return true;
+                String cityname = hotel.getCity();
+                int j = cityMapper.updatenum(cityname);
+                if (j>0){
+                    return true;
+                }
+                return false;
             }
         }
         return false;
     }
 
     @Override
+    @Transactional
     public boolean inserthotel(Hotel hotel) {
         if (hotel!=null){
             int i = hotelMapper.insertSelective(hotel);
             if (i>0){
-                return true;
+                String cityname = hotel.getCity();
+                Example example=new Example(City.class);
+                Example.Criteria city = example.createCriteria().andEqualTo("city", cityname);
+                City onecity = cityMapper.selectOneByExample(city);
+                if (onecity!=null&&onecity.getCityName()!=null){
+                    int j = cityMapper.upnum(cityname);
+                    if (i>0){
+                        return true;
+                    }
+                }else {
+                    City city1=new City();
+                    city1.setCityName(cityname);
+                    city1.setHotelnum(1);
+                    int j = cityMapper.insertSelective(city1);
+                    if (j>0){
+                        return true;
+                    }
+                }
+                return false;
             }
         }
         return false;
